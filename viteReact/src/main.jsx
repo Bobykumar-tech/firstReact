79336ca915c67f5619ea906f5e2b8055c1d2cdeb
Bobykumar <bk0184895@gmail.com>
2026-02-12 11:51:18 +0530

    second update

--- viteReact/src/main.jsx
+++ viteReact/src/main.jsx
@@ -1,9 +1,19 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function Myname(){
+    return (
+<h1>Boby nayak!</h1>
+  )
+}
+const reactElement = React.createElement(
+   'a',
+   {href : 'https://google.com' , target : '-blank'},
+   'viste to google'
+)
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  
+    reactElement
+  
 )
